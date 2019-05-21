d8d672b5884503bc18f061d891c435e8dcaa9020
Rasha Baig <rasha.baig@gmail.com>
2019-05-21 16:51:59 -0400

    added doctor component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import Navigation from './components/navigation/navigationLogIn';
 import LogInForm from './components/logInForm/LogInForm';
 import SignUpForm from './components/signUpForm/SignUpForm';
+import Doctor from './components/doctors/Doctor';
 
 // import axios from 'axios';
 
@@ -20,6 +21,7 @@ class App extends Component {
 				<Route exact path="/" render={(routerProps) => <Home {...routerProps} />} />
 				<Route exact path="/login" render={(routerProps) => <LogInForm {...routerProps} />} />
 				<Route exact path="/signup" render={(routerProps) => <SignUpForm {...routerProps} />} />
+				<Route exact path="/doctors" render={(routerProps) => <Doctor {...routerProps} />} />
 			</div>
 		);
 	}
