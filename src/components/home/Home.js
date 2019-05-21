c399765d7ed957e326fbb3d68050a4e2945eacef
Rasha Baig <rasha.baig@gmail.com>
2019-05-21 13:20:19 -0400

    finished styling dashboard, and started styling home component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import '../home/Home.css';
 
 class Home extends Component {
 	render() {
-		return <div />;
+		return (
+			<div>
+				<h1 className="homePageTitle">CareTaker</h1>
+			</div>
+		);
 	}
 }
 
