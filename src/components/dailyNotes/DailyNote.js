6e5d6c8af4549c81ce81342fea8004fe7b9c7978
Rasha Baig <rasha.baig@gmail.com>
2019-05-23 15:36:08 -0400

    added more to the note component

--- src/components/dailyNotes/DailyNote.js
+++ src/components/dailyNotes/DailyNote.js
@@ -21,11 +21,11 @@ class DailyNote extends Component {
 	}
 
 	handleSubmit() {
-		const URL = `http://localhost:3001/note/new/${this.props.userID}`;
+		const URL = `http://localhost:3001/note/new/5ce2d402236655a1648b00f5`;
 		axios
 			.put(URL, {
-				date: this.state.doctorName,
-				symptoms: this.state.doctorSpecialty
+				date: this.state.date,
+				symptoms: this.state.symptoms
 			})
 			.then(function(response) {
 				console.log(response);
@@ -40,6 +40,28 @@ class DailyNote extends Component {
 				<h1>
 					Daily Notes <img className="doctorsIcon" src={dailyNote} />
 				</h1>
+				<div className="form1">
+					<form id="doctorForm">
+						<h3>Today's date</h3>
+						<input type="date" name="date" onChange={this.handleInput} />
+						<h3>Add a note about how you're feeling today:</h3>
+						<input
+							type="text"
+							name="symptoms"
+							placeholder="log your symptoms"
+							onChange={this.handleInput}
+						/>
+						<input
+							type="submit"
+							className="doctorSubmitButton"
+							value="submit"
+							onClick={this.handleSubmit}
+						/>
+					</form>
+				</div>
+				<div>
+					<ViewAllNotes />
+				</div>
 			</div>
 		);
 	}
